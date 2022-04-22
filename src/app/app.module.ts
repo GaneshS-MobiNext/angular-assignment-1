5872ac7d003c02ff2dad4a81be4c93fc4bfd3a99
Ganesh Sonawane <ganesh.s@mobinexttech.com>
2022-04-22 10:18:36 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,31 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { ProductsComponent } from './products/products.component';
+import { AboutUsComponent } from './about-us/about-us.component';
+import { ContactUsComponent } from './contact-us/contact-us.component';
+import { ServicesComponent } from './services/services.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ProductsComponent,
+    AboutUsComponent,
+    ContactUsComponent,
+    ServicesComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    RouterModule.forRoot([
+        {path: 'home', component: HomeComponent},
+        {path: 'products', component: ProductsComponent},
+        {path: 'services', component: ServicesComponent},
+        {path: 'about', component: AboutUsComponent},
+        {path: 'contact', component: ContactUsComponent},
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
