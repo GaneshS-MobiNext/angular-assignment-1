2cd810e2af4938c083cffeb3d2516c939bea0e14
Ganesh Sonawane <ganesh.s@mobinexttech.com>
2022-04-30 17:59:12 +0530

    multiple changes

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,15 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, } from '@angular/core';
 
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.scss']
 })
+
 export class FooterComponent implements OnInit {
+  currentYear: number;
 
-  constructor() { }
-
+  constructor() { 
+    this.currentYear = new Date().getFullYear();
+  }
+  
   ngOnInit(): void {
+    
   }
 
 }
