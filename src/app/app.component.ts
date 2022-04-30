6b029940328725dab13fbb74e4ccaaed3ab83d96
Ganesh Sonawane <ganesh.s@mobinexttech.com>
2022-04-30 17:06:32 +0530

    importing json

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import * as studentsData  from '../assets/json/data.json';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'assignment-1';
+
+  students: any = (studentsData as any).default;
+
+  constructor(){}
+  ngOnInit(){
+    console.log(studentsData);
+  }
 }
