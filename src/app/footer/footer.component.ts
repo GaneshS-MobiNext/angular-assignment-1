869027630c2c39d135ba0203172e7ebbdff90eeb
GaneshS-MobiNext <ganesh.s@mobinexttech.com>
2022-05-04 11:43:14 +0530

    previous commit

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.scss']
 })
-export class FooterComponent implements OnInit {
+export class FooterComponent implements OnInit, AfterViewInit {
+  curdate:any;
 
   constructor() { }
 
   ngOnInit(): void {
+    let temp = 5;
+    this.curdate = (new Date().getMonth() + 1) + '-' + new Date().getFullYear();
+
+  }
+  ngAfterViewInit() {
+   // document.getElementById("year").innerHTML = new Date().getFullYear().toString();
   }
 
 }
